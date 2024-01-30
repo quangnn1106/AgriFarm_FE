2c7c026d22b807d1565e6e808e8e56c899f73fb9
TranTuMy <myttce160318@fpt.edu.vn>
2024-01-30 09:36:03 +0700

    update update profile page

--- src/app/[locale]/(Dashboard)/user/update/page.tsx
+++ src/app/[locale]/(Dashboard)/user/update/page.tsx
@@ -11,6 +11,7 @@ import {
   Form,
   Input,
   InputNumber,
+  Modal,
   Radio,
   Row,
   Select,
@@ -20,6 +21,7 @@ import {
   Upload
 } from 'antd';
 import React, { useState } from 'react';
+import type { UploadFile, UploadProps } from 'antd';
 
 import { PlusOutlined, CameraOutlined, HomeOutlined } from '@ant-design/icons';
 import classNames from 'classnames/bind';
@@ -38,6 +40,25 @@ const normFile = (e: any) => {
 };
 const cx = classNames.bind(styles);
 
+//config upload image
+
+type GetProps<T extends React.ComponentType<any> | object> =
+  T extends React.ComponentType<infer P> ? P : T extends object ? T : never;
+type GetProp<
+  T extends React.ComponentType<any> | object,
+  PropName extends keyof GetProps<T>
+> = NonNullable<GetProps<T>[PropName]>;
+
+type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0];
+
+const getBase64 = (file: FileType): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = error => reject(error);
+  });
+
 const UpdateUser = ({
   params
 }: {
@@ -46,6 +67,76 @@ const UpdateUser = ({
   const [form] = Form.useForm();
   const [componentDisabled, setComponentDisabled] = useState<boolean>(true);
   const t = useTranslations('UserManagement');
+
+  //Upload image
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+  const [previewTitle, setPreviewTitle] = useState('');
+  const [fileList, setFileList] = useState<UploadFile[]>([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+    },
+    {
+      uid: '-2',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+    },
+    {
+      uid: '-3',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+    },
+    {
+      uid: '-4',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+    },
+    {
+      uid: '-xxx',
+      percent: 50,
+      name: 'image.png',
+      status: 'uploading',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+    },
+    {
+      uid: '-5',
+      name: 'image.png',
+      status: 'error'
+    }
+  ]);
+
+  const handleCancel = () => setPreviewOpen(false);
+
+  const handlePreview = async (file: UploadFile) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj as FileType);
+    }
+
+    setPreviewImage(file.url || (file.preview as string));
+    setPreviewOpen(true);
+    setPreviewTitle(file.name || file.url!.substring(file.url!.lastIndexOf('/') + 1));
+  };
+
+  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) =>
+    setFileList(newFileList);
+
+  // const uploadButton = (
+  //   <button
+  //     style={{ border: 0, background: 'none' }}
+  //     type='button'
+  //   >
+  //     <PlusOutlined />
+  //     <div style={{ marginTop: 8 }}>Upload</div>
+  //   </button>
+  // );
+  // end upload image
+
   return (
     <>
       <ConfigProvider
@@ -72,40 +163,58 @@ const UpdateUser = ({
           className={cx('home-btn')}
           href='#'
           size={'large'}
+          style={{ paddingLeft: 24 }}
         >
           <HomeOutlined />
           Farm Name
         </Button>
       </ConfigProvider>
 
-      <Flex align='center' justify='space-between'>
-      <label>{t('PROFILE')}</label>
-      <Checkbox
-        checked={componentDisabled}
-        onChange={e => setComponentDisabled(e.target.checked)}
+      <Flex
+        align='center'
+        justify='space-between'
       >
-        Form disabled
-      </Checkbox>
+        <label className={cx('group-info-label')}>{t('PROFILE')}</label>
+        <Checkbox
+          checked={componentDisabled}
+          onChange={e => setComponentDisabled(e.target.checked)}
+        >
+          {t('edit_information')}
+        </Checkbox>
       </Flex>
-      
+
       <Form
         labelCol={{ span: 4 }}
         wrapperCol={{ span: 14 }}
-        layout='horizontal'
         disabled={componentDisabled}
         // style={{ maxWidth: 600 }}
       >
         <Row>
           <Col span={12}>
-            <label>{t('PROFILE_IMAGE')}</label>
+            <label className={cx('group-info-label')}>{t('PROFILE_IMAGE')}</label>
             <Form.Item
               valuePropName='fileList'
               getValueFromEvent={normFile}
+              className={cx('padding-input')}
             >
               <Upload
-                action='/upload.do'
+                action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
                 listType='picture-card'
+                onPreview={handlePreview}
+                onChange={handleChange}
               >
+                <Modal
+                  open={previewOpen}
+                  title={previewTitle}
+                  footer={null}
+                  onCancel={handleCancel}
+                >
+                  <img
+                    alt='example'
+                    style={{ width: '100%' }}
+                    src={previewImage}
+                  />
+                </Modal>
                 <button
                   style={{ border: 0, background: 'none', display: 'flex', gap: '8px' }}
                   type='button'
