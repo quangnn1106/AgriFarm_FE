b45ee9cd495d713711e692d01f0ca927b2bd33dd
quangdethuong <quangnn1106@gmail.com>
2024-02-03 18:03:56 +0700

    intergrate call registerform,add axios hook

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 export default function NotFound() {
   const router = useRouter();
   return (
-    <html lang='en'>
+    <html>
       <body>
         <Result
           status='404'
