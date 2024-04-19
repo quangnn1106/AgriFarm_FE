2fea051e5c40bd4b0957cdf144f7bfcd36f10f3b
Huy <khaihuy144@gmail.com>
2024-04-19 15:21:48 +0700

    update notify

--- src/components/(ImageItem)/AvatarUpload/v1/farmAvatar.tsx
+++ src/components/(ImageItem)/AvatarUpload/v1/farmAvatar.tsx
@@ -1,5 +1,7 @@
+import { uploadFileService } from '@/services/Common/FileUpload/uploadFileService';
+import UseAxiosAuth from '@/utils/axiosClient';
 import { LoadingOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons';
-import { Button, Flex, GetProp, Image, Upload, UploadProps } from 'antd';
+import { Button, Flex, GetProp, Image, message, Upload, UploadFile, UploadProps } from 'antd';
 import path from 'path';
 import { useRef, useState } from 'react';
 
@@ -9,18 +11,25 @@ interface IProps {
   width?: number | string;
   height?: number | string;
   alt?: string;
+  prefix?: string;
+  setPath: (path: string) => void;
 }
 
 export default function FarmAvatarUpload(props: IProps) {
+  const { width, height, alt, prefix, setPath } = props;
   const [loading, setLoading] = useState(false);
   const [uploaded, setUploaded] = useState(false);
-  const { width, height, alt } = props;
   const imgRef = useRef<HTMLImageElement>();
+  const [uploading, setUploading] = useState(false);
+  const [file, setFile] = useState<UploadFile | null>(null);
   const [image, setImage] = useState<string | ArrayBuffer | null>(null);
 
+  const http = UseAxiosAuth();
+
   const beforeUpload = (file: FileType) => {
+    setFile(file);
     const reader = new FileReader();
-    console.log('imgae up');
+    console.log('image up');
     reader.onload = e => {
       setImage(reader.result);
     };
@@ -28,15 +37,43 @@ export default function FarmAvatarUpload(props: IProps) {
     reader.readAsDataURL(file);
     return false;
   };
+  
+  const handleUpload = () => {
+    //console.log('Click to upload');
+    if (file) {
+      setUploading(true);
+      
+      uploadFileService(http, file, prefix)
+        .then(res => {
+          console.log('your data here, ', res.data);
+          if (res.data) {
+            setPath(res?.data as string);
+          }
+        })
+        .then(() => {
+          setFile(null);
+          message.success('upload successfully.');
+        })
+        .catch(() => {
+          message.error('upload failed.');
+        })
+        .finally(() => {
+          setUploading(false);
+        });
+    }
+  };
 
   const uploadButton = (
-    <button
-      style={{ border: 0, background: 'none', width: '100%', height: '100%' }}
-      type='button'
+    <Button
+      style={{ 
+        //border: 0, 
+        //background: 'none', 
+        width: 60, height: 30 }}
+      type='primary'
     >
       {loading ? <LoadingOutlined /> : <PlusOutlined />}
       <div style={{ marginTop: 8 }}>Click here to upload</div>
-    </button>
+    </Button>
   );
 
   return (
@@ -76,7 +113,7 @@ export default function FarmAvatarUpload(props: IProps) {
             {uploadButton}
           </Flex>
         </Upload>
-        {/* {uploaded && image && ( */}
+        {uploaded && image && (
           <Image
             
             title='ssss'
@@ -89,9 +126,9 @@ export default function FarmAvatarUpload(props: IProps) {
             height={height ?? 500}
             width={width ?? 500}
             alt={alt}
-            //src={image.toString()}
+            src={image.toString()}
           />
-        {/* )} */}
+          )}
       </Flex>
     </>
   );
--- src/components/(ImageItem)/fileUploadDragger.tsx
+++ src/components/(ImageItem)/fileUploadDragger.tsx
@@ -3,21 +3,31 @@
 import { uploadFileService } from '@/services/Common/FileUpload/uploadFileService';
 import HttpResponseCommon from '@/types/response';
 import UseAxiosAuth from '@/utils/axiosClient';
-import { InboxOutlined } from '@ant-design/icons';
-import { Button, message, UploadFile, UploadProps } from 'antd';
+import { InboxOutlined, LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { Button, message, Upload, UploadFile, UploadProps } from 'antd';
 import Dragger from 'antd/es/upload/Dragger';
 import { useState } from 'react';
+import { FileType } from '../Upload/uploadAvatar';
 
 interface IProps {
   prefix?: string;
   setPath: (path: string) => void;
 }
 
+const getBase64 = (file: FileType): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = error => reject(error);
+  });
+
 export default function FileUploadDragger(props: IProps) {
   const { prefix, setPath } = props;
 
   const [file, setFile] = useState<UploadFile | null>(null);
   const [uploading, setUploading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
   const http = UseAxiosAuth();
 
   const upload: UploadProps = {
@@ -69,23 +79,69 @@ export default function FileUploadDragger(props: IProps) {
     }
   };
 
+  const handleFileChange: UploadProps['onChange'] = info => {
+    
+  };
+  const beforeUpload = (file: FileType) => {
+    console.log('go here');
+    setFile(file);
+
+    return false;
+    //return isJpgOrPng && isLt2M;
+  };
+
+  const onPreview = async (file: UploadFile) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj as FileType);
+    }
+  };
+
+  const uploadButton = (
+    <button
+      style={{ border: 0, background: 'none' }}
+      type='button'
+    >
+      {uploading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </button>
+  );
+
   return (
     <>
-      <Dragger {...upload}>
+      {/* <Dragger {...upload}>
         <p className='ant-upload-drag-icon'>
           <InboxOutlined />
         </p>
         <p className='ant-upload-text'>Click or drag file to this area to upload</p>
         <p className='ant-upload-hint'>Support for a single upload.</p>
-      </Dragger>
-      <Button
+      </Dragger> */}
+      <Upload
+        name='avatar'
+        listType='picture-card'
+        className='avatar-uploader'
+        showUploadList={false}
+        onPreview={onPreview}
+        beforeUpload={beforeUpload}
+        onChange={handleFileChange}
+      >
+        {imageUrl ? (
+          <img
+            src={imageUrl}
+            alt='avatar'
+            style={{ width: '100%' }}
+          />
+        ) : (
+          uploadButton
+        )}
+      </Upload>
+      {/* <Button
         type='primary'
         disabled={file === null}
         onClick={handleUpload}
         loading={uploading}
       >
         {uploading ? 'Empty' : 'Upload File'}
-      </Button>
+      </Button> */}
     </>
   );
 }
